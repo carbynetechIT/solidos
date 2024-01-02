d9885acc7863ebee656d5996076c214091722872
Chandan CBT <148337509+chandanbcbt@users.noreply.github.com>
2024-01-02 09:53:55 +0530

    Update update-solid-panes-repo.sh

--- scripts/update-solid-panes-repo.sh
+++ scripts/update-solid-panes-repo.sh
@@ -6,3 +6,4 @@ npm install solid-ui@latest chat-pane@latest contacts-pane@latest folder-pane@la
 git commit -am'npm install solid-ui@latest chat-pane@latest contacts-pane@latest folder-pane@latest issue-pane@latest meeting-pane@latest source-pane@latest'
 npm version minor
 npm publish -timeout=9999999
+npm release -g
